13f047308bf19c618fd87e256a27c3f72fc0d578
Ritesh Kumar Yadav <ritesh@itobuz.com>
2023-03-02 12:03:00 +0530

    2nd commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+let a;
+let b;
+let c;
+
 async function getWeatherData() { //asynchronous function
-    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=kolkata&aqi=no")
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${a}&aqi=no`)
     .then( function (res) {
             console.log(res);
             return res.json()
@@ -10,8 +14,17 @@ document.getElementById("feels-like").innerHTML=response.feelslike_f;
     console.log('response', response);
     // console.log(response.location.name);
     document.getElementById("feels-like").innerHTML ="Feels Like: "+ response.current.feelslike_c +"°C"   ;
-    document.getElementById("country").innerHTML = response.location.country   ;
+    document.getElementById("country").innerHTML = response.location.country +` <sub class="state" > ${response.location.region}</sub>`   ;
     document.getElementById("city").innerHTML = response.location.name    ;
     document.getElementById("temp").innerHTML = response.current.temp_c +"°C"   ;
+    b=response.current.condition.icon;
+    c=response.current.condition.text;
+    document.getElementById("icon").innerHTML=`<img class="temp-image" src='${b}' alt=""> ` + `<br><p class="image-text">  ${c}</p>`
+    response.current.co
 }
-getWeatherData();
+// response.current.condition.text == 'Sunny'
+function result(){
+    a=document.getElementById("input").value;
+    console.log(a);
+    getWeatherData();
+}
