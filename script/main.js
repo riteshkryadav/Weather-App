c9b9922da6ddb258db0619cea4035b60964eaea2
Ritesh Kumar Yadav <ritesh@itobuz.com>
2023-03-22 18:29:34 +0530

    version5

--- script/main.js
+++ script/main.js
@@ -32,44 +32,23 @@ for (let i = 0; i < dropDown.length; i++) {
 //asynchronous function
 async function getWeatherData() { 
     const response = await fetch(
-        // `http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${city}&aqi=no`
-        `http://localhost:8000/about`
+             `http://localhost:8000/about`
         )
         .then(function (res) {
             console.log(res);
-            return res.json()
-
-
-
+            return res.json();
         }
 
         );
         for(let i = 0;i<response.length;i++){
             if(response[i].location === dropdown_city) {
                 console.log(response[i]);
-                // console.log('response', response);
-                document.getElementById("temp").innerHTML = response[i].temp_c + "°";
-                document.getElementById("feels-like").innerHTML = "Feels  " + response[i].feelsLikeC + "°";
+                document.getElementById("temp").innerHTML = response[i].temp_c + "°C";
+                document.getElementById("feels-like").innerHTML = "Feels  " + response[i].feelsLikeC + "°C";
                 document.getElementById("icon").innerHTML = `<img src='./images/partly-claudy.png' alt=""> `
                 document.getElementById("input").value = response[i].location;
             }
         }
-        // console.log(response[0]);
-        // console.log('response', response);
-        // document.getElementById("temp").innerHTML = response[0].temp_c + "°";
-        // document.getElementById("feels-like").innerHTML = "Feels  " + response[0].feelsLikeC + "°";
-        // document.getElementById("icon").innerHTML = `<img src='./images/partly-claudy.png' alt=""> `
-        // document.getElementById("input").value = response[0].location;
-    
-
-    //     );
-    // document.getElementById("feels-like").innerHTML = response.feelslike_f;
-    // console.log('response', response);
-    // document.getElementById("temp").innerHTML = response.current.temp_f + "°";
-    // document.getElementById("feels-like").innerHTML = "Feels  " + response.current.feelslike_f + "°";
-    // document.getElementById("icon").innerHTML = `<img src='./images/partly-claudy.png' alt=""> `
-    // document.getElementById("input").value = response.location.name;
-
 }
 document.getElementById("input").addEventListener('keyup',
     function (event) {
